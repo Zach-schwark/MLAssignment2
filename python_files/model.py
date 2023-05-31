0383989776dcd11c91baff9dade43a9856fe7592
Zach SChwark <zachs1708@gmail.com>
2023-05-31 20:19:31 +0200

    add evaluation

--- python_files/model.py
+++ python_files/model.py
@@ -5,49 +5,98 @@ import preprocessing as myData
 (x_train, y_train) = myData.getTrainingData()
 (x_validation, y_validation) = myData.getValidationData()
 (x_test, y_test) = myData.getTestingData()
-print(len(x_train[0]))
-print(tf.__version__)
-
-modelStructure = [71,71,10]
-activationFunction = "sigmoid"
-numEpochs = 5
-learningRate = 0.1
-regularization = 0.1
-optimizer = tf.keras.optimizers.SGD(learning_rate=learningRate)
-loss_fn = tf.keras.losses.LogCosh(reduction=tf.keras.losses.Reduction.SUM)
-
-#setting up model
-layers = [tf.keras.layers.InputLayer(input_shape=(modelStructure[0],),ragged=False)]
-for i in range(1, len(modelStructure)):
-        layers.append(tf.keras.layers.Dense(modelStructure[i], activation= activationFunction, bias_initializer= "ones",activity_regularizer=tf.keras.regularizers.L1(regularization)))
-        
-                
-#initilizing model
-model = tf.keras.models.Sequential(layers)
-
-#gets predictions of untrained model/ first forward propagate
-predictions = model(x_train[:1]).numpy()
+f = open("evaluation.txt", "w")
+f.write("")
+f.close()
 
 
-#normalizing out put, to get ptedictions as probabilities
-predictions = tf.nn.softmax(predictions).numpy()
+# research into dropout
+LR_arr = [0.1, 0.15, 0.2, 0.25]
+Reg_arr = [0.01, 0.03, 0.06, 0.09]
+HLnodes_arr = [54, 20, 70, 120]
+actFunc_arr = ["sigmoid", "relu","tanh","softsign"]
+epochs_arr = [10, 15, 20, 25]
+
+# using list comprehension
+# to compute all possible permutations
+permutations = [[i, j, k,w,z] for i in HLnodes_arr
+                for j in actFunc_arr
+                for k in epochs_arr
+                for w in LR_arr
+                for z in Reg_arr]
+for p in range(len(permutations)):
+    modelStructure =permutations[p][0]
+    activationFunction = permutations[p][1]
+    numEpochs =  permutations[p][2]
+    learningRate =  permutations[p][3]
+    regularization =  permutations[p][4]
+    optimizer = tf.keras.optimizers.SGD(learning_rate=learningRate)
+    loss_fn = tf.keras.losses.LogCosh(reduction=tf.keras.losses.Reduction.SUM)
+
+    #setting up model
+    layers = [tf.keras.layers.InputLayer(input_shape=(71,),ragged=False)]
+    layers.append(tf.keras.layers.Dense(modelStructure, activation= activationFunction, bias_initializer= "ones",activity_regularizer=tf.keras.regularizers.L1(regularization)))
+    layers.append(tf.keras.layers.Dense(10, activation= activationFunction, bias_initializer= "ones",activity_regularizer=tf.keras.regularizers.L1(regularization)))
 
 
-#calculate last layers error/ final error
-loss_fn(y_train[:1], predictions).numpy()
+    #initilizing model
+    model = tf.keras.models.Sequential(layers)
+    model.summary()
+    print(model.layers[0])
+    #gets predictions of untrained model/ first forward propagate
+    predictions = model(x_train[:1]).numpy()
 
 
-# "backpropogates" and compiles model with deltas annd gradeints etc....sets this stuffup for training
-model.compile(optimizer= optimizer,
-              loss=loss_fn,
-              metrics=['accuracy'])
+    #normalizing out put, to get ptedictions as probabilities
+    predictions = tf.nn.softmax(predictions).numpy()
+    #predictions_final = np.zeros(len(predictions), dtype=int)
+    #for i in range(len(predictions)):
+    #        #print(len(np.where(predictions[i]==max(predictions[i]))))
+    #        if(len(np.where(predictions[i]==max(predictions[i])))>=1):
+    #                predictions_final[i]=int(np.where(predictions[i]==max(predictions[i]))[0][0])
+    #        else:
+    #                #print("hi")
+    #                predictions_final[i]=int(np.where(predictions[i]==max(predictions[i]))[0])
+    #
+    #calculate last layers error/ final error
+    loss_fn(y_train[:1], predictions).numpy()
 
 
-# trains the model on training data
-history = model.fit(x_train, y_train, epochs=numEpochs)
+    # "backpropogates" and compiles model with deltas annd gradeints etc....sets this stuffup for training
+    model.compile(optimizer= optimizer,
+                  loss=loss_fn,
+                  metrics=['accuracy'])
 
-# evaluates and validates model on validation data
-model.evaluate(x_validation,  y_validation, verbose=2)
+
+    earlystopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss",
+                                            mode="min", patience=5,
+                                            restore_best_weights=True)
+
+    print(model.weights[0])
+    # trains the model on training data
+    #history = model.fit(x_train, y_train, epochs=numEpochs)
+    history = model.fit(x_train, y_train, epochs=numEpochs,validation_data=(x_validation,  y_validation),callbacks=[earlystopping])
+    model.summary()
+    print(model.weights[0])
+    f = open("evaluation.txt", "a")
+    f.write("\n \n Hyper Parameters:\n modelStructure ="+str(permutations[p][0])+"\n activationFunction = "+str(permutations[p][1])+"\n numEpochs = "+str(permutations[p][2]) +"\n learningRate ="+str(permutations[p][3])+"\n regularization = "+str(permutations[p][4])+"\n")
+    f.write("loss="+str(model.evaluate(x_validation,  y_validation, verbose=2)[0])+"\t accuracy= "+str(model.evaluate(x_validation,  y_validation, verbose=2)[1])+ "\n")
+    f.close()
+    # evaluates and validates model on validation data
+    model.evaluate(x_validation,  y_validation, verbose=2)
+
+
+# evaluates on testing data
+#model.evaluate(x_test,  y_test, verbose=2)
+config = model.get_config()
+model.save(filepath = "model.keras",save_format='tf')
+
+
+
+# change stuff using validating
+# final test usingg testing 
+
+
 
 #predictions_test = model.predict_on_batch(x_test)
 #predictions_test = tf.nn.softmax(predictions_test).numpy()
@@ -59,25 +108,3 @@ model.evaluate(x_validation,  y_validation, verbose=2)
 #        else:
 #                #print(np.where(predictions_test[i]==max(predictions_test[i]))[0])
 #                predictions_final[i]=int(np.where(predictions_test[i]==max(predictions_test[i]))[0])
-#print(predictions_final)
-
-# evaluates on testing data
-#model.evaluate(x_validation,  y_validation, verbose=2)
-config = model.get_config()
-model.save(filepath = "model.keras",save_format='tf')
-
-
-
-#things to consider changing to optimise
-
-# model structure, layers nodes etc
-# hyperparameters : learning rate, regularisation, number of epochs
-# activation function
-# loss function 
-# how outputted
-
-# change stuff using validating
-# final test usingg testing 
-
-
-# research into dropout
